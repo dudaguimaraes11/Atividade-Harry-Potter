5f9b40ee0fe68c1770999cc746d14b8245da0673
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-08-25 09:26:09 -0300

    Feat: Adiciona continuação do projeto de Harry Potter com Node

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 import express from "express";
-import bruxos from "./src/data/bruxos.js";
+
+//Adiciona essa linha para importar dados
+import dados from "./src/data/dados.js"
 
 const app = express();
-const serverPort = 3000; 
-
 app.use (express.json());
 
+const { bruxos, casas, varinhas, animais, pocoes} = dados; 
+const serverPort = 3000; 
+
+// Rota principal GET para "/"
 app.get("/", (req, res) => {
     res.send("<h1> Vamos de Harry Potter </h1>");
 });
 
+// Criar a rota usando ela 
 app.get("/bruxos", (req, res) => {
-    res.json(bruxos);
+    res.status(200).json(bruxos);
 });
 
+// Criar rota usando casas 
+app.get ("/casas", (req, res) => {
+    res.status(200).json(casas); 
+})
 
-app.listen(3000, () => {
+// Criar rota usando varinhas 
+app.get ("/varinhas", (req, res) => {
+    res.status(200).json(varinhas); 
+})
+
+// Criar rota usando animais 
+app.get ("/animais", (req, res) => {
+    res.status(200).json(animais); 
+})
+
+// Criar rota usando pocoes 
+app.get ("/pocoes", (req, res) => {
+    res.status(200).json(pocoes); 
+})
+
+app.listen(serverPort, () => {
     console.log(`🧙‍♂️ API dos Bruxos está no ar na porta 3000, http://localhost:${serverPort}`);
 });
---
+++ src/data/dados.js
@@ -0,0 +1,213 @@
+const bruxos = [
+        {
+        id: 1,
+        nome: "Harry Potter",
+        casa: "Grifinória",
+        ano: 7,
+        varinha: "Azevinho e pena de fênix, 28cm",
+        mascote: "Coruja (Edwiges)",
+        patrono: "Cervo",
+        especialidade: "Sobreviver a Voldemort 😎"
+    },
+    {
+        id: 2,
+        nome: "Hermione Granger",
+        casa: "Grifinória",
+        ano: 7,
+        varinha: "Videira e fibra de coração de dragão, 27cm",
+        mascote: "Gato (Bichento)",
+        patrono: "Lontra",
+        especialidade: "Saber tudo de tudo"
+    }, 
+     {
+        id: 3,
+        nome: "Ron Weasley",
+        casa: "Grifinória",
+        ano: 7,
+        varinha: "Salgueiro e crina de unicórnio, 35cm",
+        mascote: "Rato (Perebas)",
+        patrono: "Terrier Jack Russell",
+        especialidade: "Xadrez Bruxo",
+    },
+    {
+        id: 4,
+        nome: "Draco Malfoy",
+        casa: "Sonserina",
+        ano: 7,
+        varinha: "Espinheiro e crina de unicórnio, 25cm",
+        mascote: "Coruja (não nomeada)",
+        patrono: "Desconhecido",
+        especialidade: "Ser irritante",
+    },
+        {
+        id: 5,
+        nome: "Luna Lovegood",
+        casa: "Corvinal",
+        ano: 6,
+        varinha: "Madeira de cerejeira e núcleo desconhecido",
+        mascote: "Nenhum",
+        patrono: "Lebre",
+        especialidade: "Ver criaturas imaginárias (ou não) 👻",
+    },
+    {
+        id: 6, 
+        nome: "Neville Longbottom",
+        casa: "Grifinória",
+        ano: 7,
+        varinha: "Cerejeira e pelo de unicórnio, 30cm",
+        mascote: "Sapo (Trevor)",
+        patrono: "Não conhecido",
+        especialidade: "Herbologia avançada",
+    },
+    {
+        id: 7, 
+        nome: "Cho Chang",
+        casa: "Corvinal",
+        ano: 7,
+        varinha: "Salgueiro e fibra de coração de dragão, 23cm",
+        mascote: "Coruja (não nomeada)",
+        patrono: "Cisne",
+        especialidade: "Quadribol (Apanhadora)",
+    },
+    {
+    
+            id: 8, 
+            nome: "Cedrico Diggory",
+            casa: "Lufa-Lufa",
+            ano: 7,
+            varinha: "Freixo e pelo de unicórnio, 32cm",
+            mascote: "Nenhum",
+            patrono: "Não conhecido",
+            especialidade: "Cavalheirismo",
+    
+        
+    },
+    {
+        id: 9, 
+        nome: "Bellatrix Lestrange",
+        casa: "Sonserina",
+        ano: 6,
+        varinha: "Nogueira e fibra de coração de dragão, 32cm",
+        mascote: "Nenhum",
+        patrono: "Não conhecido",
+        especialidade: "Artes das Trevas",
+    },
+    {
+        id: 10, 
+        nome: "Minerva McGonagall",
+        casa: "Grifinória",
+        ano: 9,
+        varinha: "Abeto e fibra de coração de dragão, 24cm",
+        mascote: "Gato",
+        patrono: "Gatoo",
+        especialidade: "Transfiguração",
+    }
+]
+
+const casas = [
+    {
+        id: 1, 
+        nome: "Grifinória", 
+        fundador: "Godric Griffindor",
+        cores: "Vermelho e Dourado",
+        animal: "Leao"
+    }, 
+    {
+        id: 2, 
+        nome: "Sonserina", 
+        fundador: "Salazar Sonserina", 
+        cores: "Verde e Prata", 
+        animal: "Serpente"
+    },
+    {
+        id: 3, 
+        nome: "Corvinal", 
+        fundador: "Rowena Ravenclaw", 
+        cores: "Azul e Bronze", 
+        animal: "Águia"
+    },
+    {
+        id: 4, 
+        nome: "Lufa-Lufa", 
+        fundador: "Helga Hufflepuff", 
+        cores: "Amarelo e Preto", 
+        animal: "Texugo"
+    }
+]
+
+const varinhas = [
+    {
+        id: 1, 
+        material: "Azevinho", 
+        núcleo: "Pena de Fênix",
+        comprimento: "28cm"
+    }, 
+    {
+        id: 2, 
+        material: "Videira", 
+        núcleo: "Fibra de Coração de Dragão",
+        comprimento: "27cm"
+    }, 
+    {
+         id: 3, 
+        material: "Salgueiro", 
+        núcleo: "Pelo de Unicórnio",
+        comprimento: "35cm"
+    }, 
+    {
+        id: 4, 
+        material: "Carvalho inglês", 
+        núcleo: "Pelo de Testrálio",
+        comprimento: "32cm"
+    }
+]
+
+const animais = [
+    {
+        id: 1, 
+        nome: "Coruja",
+        tipo: "Correio/Companhia"
+    },
+    {
+        id: 2, 
+        nome: "Gato",
+        tipo: "Companhia"
+    },
+    {
+        id: 3, 
+        nome: "Sapo",
+        tipo: "Companhia"
+    }, 
+    {
+        id: 4, 
+        nome: "Rato",
+        tipo: "Companhia"
+    }
+]
+
+const pocoes = [
+    {
+        id: 1, 
+        nome: "Polissulco", 
+        efeito: "Transforma na aparência de outra pessoa"
+    }, 
+    {
+        id: 2, 
+        nome: "Felix Felicis", 
+        efeito: "Sorte temporária"
+    }, 
+    {
+        id: 3, 
+        nome: "Amortentia", 
+        efeito: "Poção do Amor"
+    }, 
+    {
+        id: 4, 
+        nome: "Veritaserum", 
+        efeito: "Força a dizer a verdade"
+    }, 
+]
+
+export default {bruxos, casas, varinhas, animais, pocoes}; 
+
+ 
